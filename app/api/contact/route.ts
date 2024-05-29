a1312632ff420e2373f7073601d0425fd730259a
shohag <shohagmiah2100@gmail.com>
2024-05-29 20:29:48 +0600

    final commit

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,38 +1,38 @@
+import nodemailer from "nodemailer";
 
-import nodemailer from 'nodemailer';
+export async function POST(req: Request, res: Response) {
+  const { name, email, message } = await req.json();
+  console.log(name, email, message);
 
-export async function POST(req:Request, res:Response) {
-    const { name, email, message } = await req.json();
-    console.log(name, email, message)
+  // Set up the Nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER, // Your email address
+      pass: process.env.GMAIL_PASS, // Your email password
+    },
+  });
 
-    // Set up the Nodemailer transporter
-    const transporter = nodemailer.createTransport({
-      service: 'gmail', // Use your email service
-      auth: {
-        user: process.env.GMAIL_USER, // Your email address
-        pass: process.env.GMAIL_PASS  // Your email password
-      }
-    });
-
-    // Set up email data
-    const mailOptions = {
-      from: email,
-      to: process.env.RECIPIENT_EMAIL, // Your email address to receive the message
-      subject: `New message from ${name}`,
-      text: `
+  // Set up email data
+  const mailOptions = {
+    from: email,
+    to: process.env.RECIPIENT_EMAIL, // Your email address to receive the message
+    subject: `New message from ${name}`,
+    text: `
         Name: ${name}
         Email: ${email}
         Message: ${message}
       `,
-    };
+  };
 
-    // Send email
-    try {
-      await transporter.sendMail(mailOptions);
-     return Response.json({ success: true });
-    } catch (error) {
-      console.error('Error sending email:', error);
-     return Response.json({ success: false });
-    }
-};
-
+  // Send email
+  try {
+    await transporter.sendMail(mailOptions);
+    return Response.json({ success: true });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return Response.json({ success: false });
+  }
+}
