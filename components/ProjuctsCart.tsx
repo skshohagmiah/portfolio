a05b364bca31cc8e2c56d37c20a8ace61fcc8dde
shohag <shohagmiah2100@gmail.com>
2024-05-30 10:06:36 +0600

    dark mode problem solve

--- components/ProjuctsCart.tsx
+++ components/ProjuctsCart.tsx
@@ -61,14 +61,14 @@ export function ThreeDCardDemo() {
             <CardBody className="relative group/card  hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-black  border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
               <CardItem
                 translateZ="50"
-                className="text-xl font-bold text-neutral-600 dark:text-white"
+                className="text-xl font-bold text-white"
               >
                 {item.name}
               </CardItem>
               <CardItem
                 as="p"
                 translateZ="60"
-                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                className="text-sm max-w-sm mt-2 text-neutral-300"
               >
                 {item.description}
               </CardItem>
@@ -87,7 +87,7 @@ export function ThreeDCardDemo() {
                   as={Link}
                   href={item.github}
                   target="__blank"
-                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  className="px-4 py-2 rounded-xl text-xs font-normal text-white"
                 >
                   Github
                 </CardItem>
@@ -95,7 +95,7 @@ export function ThreeDCardDemo() {
                   translateZ={20}
                   as={Link}
                   href={item.live}
-                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                  className="px-4 py-2 rounded-xl bg-white text-black text-xs font-bold"
                 >
                   Live Link
                 </CardItem>
